5447ae9375057549129b00fa894fd9805f21eb03
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2023-01-13 09:55:38 +0800

    end state bug demo

--- src/routes/carousel.tsx
+++ src/routes/carousel.tsx
@@ -27,37 +27,37 @@ const images = [
 ];
 
 const collapsedAspectRatio = 1 / 3;
-const expandedAspectRatio = 3 / 2;
+const fullAspectRatio = 3 / 2;
+const gap = 2;
+const margin = 12;
 
-export default function Carousel() {
+export default function Page() {
   const [index, setIndex] = useState(0);
 
-  useKeypress("ArrowLeft", () => {
-    if (index > 0) {
-      setIndex(index - 1);
-    }
-  });
   useKeypress("ArrowRight", () => {
     if (index + 1 < images.length) {
       setIndex(index + 1);
     }
   });
 
+  useKeypress("ArrowLeft", () => {
+    if (index > 0) {
+      setIndex((i) => i - 1);
+    }
+  });
+
   return (
     <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
-      <div className="h-screen bg-black">
-        <div className="mx-auto flex h-full flex-col items-center justify-center">
-          <div className="relative max-w-[500px] overflow-hidden">
-            <motion.div
-              initial={false}
-              animate={{ x: `-${index * 100}%` }}
-              className="flex"
-            >
-              {images.map((image) => (
-                <img
+      <div className="h-full bg-black">
+        <div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
+          <div className="relative overflow-hidden">
+            <motion.div animate={{ x: `-${index * 100}%` }} className="flex">
+              {images.map((image, i) => (
+                <motion.img
                   key={image}
                   src={image}
-                  className="aspect-3/2 object-cover"
+                  animate={{ opacity: i === index ? 1 : 0.3 }}
+                  className="aspect-[3/2] object-cover"
                 />
               ))}
             </motion.div>
@@ -68,58 +68,69 @@ export default function Carousel() {
                   animate={{ opacity: 0.7 }}
                   exit={{ opacity: 0, pointerEvents: "none" }}
                   whileHover={{ opacity: 1 }}
-                  className="absolute left-2 top-1/2 -mt-4 flex h-10 w-10 items-center justify-center rounded-full bg-white"
+                  className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
                   onClick={() => setIndex(index - 1)}
                 >
-                  <ChevronLeftIcon className="mr-1 h-6 w-6" />
+                  <ChevronLeftIcon className="h-6 w-6" />
                 </motion.button>
               )}
             </AnimatePresence>
+
             <AnimatePresence initial={false}>
               {index + 1 < images.length && (
                 <motion.button
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 0.7 }}
-                  exit={{ opacity: 0 }}
+                  exit={{ opacity: 0, pointerEvents: "none" }}
                   whileHover={{ opacity: 1 }}
-                  className="absolute right-2 top-1/2 -mt-4 flex h-10 w-10 items-center justify-center rounded-full bg-white"
+                  className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
                   onClick={() => setIndex(index + 1)}
                 >
-                  <ChevronRightIcon className="ml-1 h-6 w-6" />
+                  <ChevronRightIcon className="h-6 w-6" />
                 </motion.button>
               )}
             </AnimatePresence>
           </div>
-          <div>
-            <div className="r absolute inset-x-0 bottom-6 flex justify-center overflow-hidden">
-              <motion.div className="flex h-14 w-24 justify-center gap-1">
-                {images.map((image, i) => (
-                  <motion.button
-                    animate={index === i ? "active" : "inactive"}
-                    variants={{
-                      active: {
-                        aspectRatio: expandedAspectRatio,
-                        marginLeft: 10,
-                        marginRight: 10,
-                        opacity: 1,
-                      },
-                      inactive: {
-                        aspectRatio: collapsedAspectRatio,
-                        marginLeft: 0,
-                        marginRight: 0,
-                        opacity: 0.8,
-                      },
-                    }}
-                    whileHover={{ aspectRatio: 3 / 2, opacity: 1 }}
-                    onClick={() => setIndex(i)}
-                    className={` h-full shrink-0`}
-                    key={image}
-                  >
-                    <img src={image} className={`h-full object-cover `} />
-                  </motion.button>
-                ))}
-              </motion.div>
-            </div>
+
+          <div className="absolute inset-x-0 bottom-6 flex justify-center overflow-hidden">
+            <motion.div
+              initial={false}
+              animate={{
+                x: `-${
+                  index * 100 * (collapsedAspectRatio / fullAspectRatio) +
+                  index * gap +
+                  margin
+                }%`,
+              }}
+              style={{ aspectRatio: fullAspectRatio, gap: `${gap}%` }}
+              className="flex h-14"
+            >
+              {images.map((image, i) => (
+                <motion.button
+                  key={image}
+                  onClick={() => setIndex(i)}
+                  whileHover={{ opacity: 1 }}
+                  initial={false}
+                  animate={i === index ? "active" : "inactive"}
+                  variants={{
+                    active: {
+                      marginLeft: `${margin}%`,
+                      marginRight: `${margin}%`,
+                      opacity: 1,
+                      aspectRatio: fullAspectRatio,
+                    },
+                    inactive: {
+                      marginLeft: "0%",
+                      marginRight: "0%",
+                      opacity: 0.5,
+                      aspectRatio: collapsedAspectRatio,
+                    },
+                  }}
+                >
+                  <motion.img src={image} className="h-full object-cover" />
+                </motion.button>
+              ))}
+            </motion.div>
           </div>
         </div>
       </div>
